de8b5d3e8598ed33847679589cb1028111cdfa09
Filip Dujmusic <filip@ampnet.io>
2021-06-11 10:32:48 +0200

    Basic identity registry implementation.

--- contracts/IdentityProvider.sol
+++ contracts/IdentityProvider.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract IdentityProvider {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract IdentityProvider is Ownable {
 
     struct Identity {
         address wallet;
@@ -9,6 +11,14 @@ contract IdentityProvider {
         bool whitelisted;
     }
 
-    mapping(address => Identity) public id; 
+    mapping(address => Identity) public id;
+
+    event IdentityUpdated(address indexed wallet, bool whitelisted);
+
+    function update(address wallet, string memory ipfsInfo, bool whitelisted) external onlyOwner {
+        id[wallet].whitelisted = whitelisted;
+        id[wallet].ipfsInfo = ipfsInfo;
+        emit IdentityUpdated(wallet, whitelisted);
+    }
     
 }
